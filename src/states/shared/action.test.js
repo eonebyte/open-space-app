0e17f684cb2ea71196b2f1ed1ebfa127abb263a0
Eone Byte <eonebyte@gmail.com>
2023-09-17 14:29:17 +0700

    make test pass

--- src/states/shared/action.test.js
+++ src/states/shared/action.test.js
@@ -54,10 +54,6 @@ describe('asyncPopulateUsersAndTalks thunk', () => {
     delete api._getAllUsers;
     delete api._getAllTalks;
   });
-});
-
-describe('asyncPopulateUsersAndTalks thunk', () => {
-  // ... backup and restore
 
   it('should dispatch action correctly when data fetching success', async () => {
     // arrange
@@ -75,5 +71,24 @@ describe('asyncPopulateUsersAndTalks thunk', () => {
     expect(dispatch).toHaveBeenCalledWith(receiveTalksActionCreator(fakeTalksResponse));
     expect(dispatch).toHaveBeenCalledWith(receiveUsersActionCreator(fakeUsersResponse));
     expect(dispatch).toHaveBeenCalledWith(hideLoading());
+  });
+
+  it('should dispatch action and call alert correctly when data fetching failed', async () => {
+    // arrange
+    // stub implementation
+    api.getAllUsers = () => Promise.reject(fakeErrorResponse);
+    api.getAllTalks = () => Promise.reject(fakeErrorResponse);
+    // mock dispatch
+    const dispatch = vi.fn();
+    // mock alert
+    window.alert = vi.fn();
+
+    // action
+    await asyncPopulateUsersAndTalks()(dispatch);
+
+    // assert
+    expect(dispatch).toHaveBeenCalledWith(showLoading());
+    expect(dispatch).toHaveBeenCalledWith(hideLoading());
+    expect(window.alert).toHaveBeenCalledWith(fakeErrorResponse.message);
   });
 });
--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,11 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+/* eslint-disable import/no-extraneous-dependencies */
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-	plugins: [react()],
-})
+  plugins: [react()],
+  test: {
+    environment: 'jsdom',
+  },
+});
